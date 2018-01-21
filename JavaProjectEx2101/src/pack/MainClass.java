b9ef4a0c9e9dc8ab59b6120d6cefa69a3c1dd5fc
Akash D <akash.dambale1@gmail.com>
2018-01-21 13:20:48 +0530

    added 2.1, 2.2

--- JavaProjectEx2101/src/pack/MainClass.java
+++ JavaProjectEx2101/src/pack/MainClass.java
@@ -6,7 +6,8 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		
 		System.out.println("hi 1");
-		System.out.println("hi 2");
+		System.out.println("hi 2.1");
+		System.out.println("hi 2.2");
 
 	}
 
